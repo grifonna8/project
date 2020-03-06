a9fae82c2ede4bd6ed7de74fd6a5f60d58a16652
grifonna8 <grifonna8@gmail.com>
2020-03-06 23:18:32 +0300

    made slider

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
   'use strict';
-// Timer вариант 2 с setInterval
-  function countTimer(deadline){
-    let timerHours = document.querySelector('#timer-hours'),
+  // Timer вариант с setInterval
+  const countTimer = (deadline) => {
+    const timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds'),
-        timer, idInterval;
-    function getTimeRemaining(){
-      let dateStop = new Date(deadline).getTime(),
+        timerSeconds = document.querySelector('#timer-seconds');
+    let timer, idInterval;
+    const getTimeRemaining = () => {
+      const dateStop = new Date(deadline).getTime(),
           dateNow = new Date().getTime(),
           timeRemaining = (dateStop - dateNow) / 1000, /* перевели мс в сек */
           seconds = Math.floor(timeRemaining % 60),
           minutes = Math.floor((timeRemaining / 60) % 60),
           hours = Math.floor(timeRemaining / 60 / 60);
       return {timeRemaining, hours, minutes, seconds};
-    }
+    };
 
-    function timeCheck(){
+    const timeCheck = () => {
       timer = getTimeRemaining();
-    } 
-    function updateClock(){
+    };
+
+    const updateClock = () => {
       timeCheck();
       if (timer.hours >= 0 && timer.hours <=9){
         timerHours.textContent = '0' + timer.hours;
@@ -42,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function(){
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
       }
-    }
+    };
 
     updateClock();
     if(timer.timeRemaining > 0){
@@ -52,8 +53,233 @@ document.addEventListener('DOMContentLoaded', function(){
       timerMinutes.textContent = '00';
       timerSeconds.textContent = '00';
     }
-  }
+  };
   countTimer('04 march 2020');
   
+  // меню
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          menuItems = menu.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    menu.addEventListener('click', (event) => {
+      let target = event.target;
+      if(target.classList.contains('close-btn')){
+        handlerMenu();
+      } else {
+        target = target.closest('menu');
+        if (target){
+          menuItems.forEach((elem) =>{
+            handlerMenu();
+          });
+        }
+      }
+    });
+
+  };
+  toggleMenu();
+
+  // popup
+  const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
+          popupContent = document.querySelector('.popup-content'),
+          width = document.documentElement.clientWidth;
+    let monitorCenter, popupContentWidth;
+    
+    // let popupAnimate = () => {
+    //   popupContent.style.left = '0';
+    //     animInterval = requestAnimationFrame(popupAnimate);
+    //     for(let i = 0; i <= width / 2; i++){
+    //       popupContent.style.left = i + 'px';
+    //     }
+    // };
+
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+        function animate(draw, duration=1200){
+          let startTime = performance.now();
+          function step(currentTime){
+            let progress = (currentTime - startTime) / duration;
+            draw(progress);
+            if(progress < 1) {
+              requestAnimationFrame(step);
+            }
+          }
+          requestAnimationFrame(step);
+        }
+        function animateRes(begin, end){
+          animate(
+            (progress) => {
+              popupContent.style.left = Math.ceil((end - begin) * progress + begin) + 'px';
+            }
+          );
+        }
+        popupContentWidth = popupContent.offsetWidth;
+        monitorCenter = width / 2 - popupContentWidth / 2;
+        if(width > 768){
+          console.log(width);
+          animateRes(0, monitorCenter);
+          popupContent.style.left = '0';
+        }
+
+      });
+    });
+    
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+    });
+
+  };
+
+  togglePopup();
+
+  // Табы
+  const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+    tab = tabHeader.querySelectorAll('.service-header-tab'),
+    tabContent = document.querySelectorAll('.service-tab');
+
+    const toggleTabContent = (index) => {
+      for(let i = 0; i < tabContent.length; i++){
+        if(index === i){
+          tab[i].classList.add('active');
+          tabContent[i].classList.remove('d-none');
+        } else {
+          tab[i].classList.remove('active');
+          tabContent[i].classList.add('d-none');
+        }
+      }
+    };
+
+    tabHeader.addEventListener('click', (event) => {
+      // let target = event.target;
+      /* вариант 1 */
+      // while (target !== tabHeader){
+      //   if(target.classList.contains('service-header-tab')){
+      //     tab.forEach((item, i) => {
+      //       if(item === target){
+      //         toggleTabContent(i);
+      //       }
+      //     });
+      //     return;
+      //   }
+      //   target = target.parentNode; 
+      /* если кликнули на span, то даем ему родителя span, 
+        а значит на 2 проходе уже попадем в условие содержит ли service-header-tab*/
+      // }
+
+      /* вариант 2 с closest*/
+      let target = event.target;
+      target = target.closest('.service-header-tab'); /* идет к родителю выше, поднимается по цепочке до 
+      документа вверх, если найдет, то вернет тот элемент, который содержит этот селектор, 
+      если же совсем ничего нет, то null */
+
+      if(target){
+        tab.forEach((item, i) => {
+          if(item === target){
+            toggleTabContent(i);
+          }
+        });
+      }
+      /* если кликнули на span, то даем ему родителя span, 
+        а значит на 2 проходе уже попадем в условие содержит ли service-header-tab*/
+      // }
+      
+    });
+  };
+  tabs();
+
+  // слайдер
+  const slider = () => {
+    const slide = document.querySelectorAll('.portfolio-item'),
+          btn = document.querySelectorAll('.portfolio-btn'),
+          dot = document.querySelectorAll('.dot'),
+          slider = document.querySelector('.portfolio-content');
+    let currentSlide = 0, 
+        interval;
+
+    const prevSlide = (elem, index, strClass) => {
+      elem[index].classList.remove(strClass);
+    };
+
+    const nextSlide = (elem, index, strClass) => {
+      elem[index].classList.add(strClass);
+    };
+
+    const autoplaySlide = () => {
+      prevSlide(slide, currentSlide, 'portfolio-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+      currentSlide++;
+      if(currentSlide >= slide.length){
+        currentSlide = 0;
+      }
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
+    };
+
+    const startSlide = (time=3000) => {
+      interval = setInterval(autoplaySlide, time);
+    };
+
+    const stopSlide = () => {
+      clearInterval(interval);
+    };
+
+    slider.addEventListener('click', (event) =>{
+      event.preventDefault();
+      let target = event.target;
+
+      if(!target.matches('.portfolio-btn, .dot')){
+        return;
+      }
+      prevSlide(slide, currentSlide, 'portfolio-item-active');
+      prevSlide(dot, currentSlide, 'dot-active');
+
+      if(target.matches('#arrow-right')){
+        currentSlide++;
+      } else if (target.matches('#arrow-left')){
+        currentSlide--;
+      } else if (target.matches('.dot')){
+        dot.forEach((elem, index) => {
+          if (elem === target){
+            currentSlide = index;
+          }
+        });
+      }
+      if(currentSlide >= slide.length){
+        currentSlide = 0;
+      }
+      if(currentSlide < 0){
+        currentSlide = slide.length - 1;
+      }
+      nextSlide(slide, currentSlide, 'portfolio-item-active');
+      nextSlide(dot, currentSlide, 'dot-active');
+    });
+
+    slider.addEventListener('mouseover', (event) => {
+      if(event.target.matches('.portfolio-btn') || 
+      event.target.matches('.dot')){
+        stopSlide();
+      }
+    });
+    slider.addEventListener('mouseout', (event) => {
+      if(event.target.matches('.portfolio-btn') || 
+      event.target.matches('.dot')){
+        startSlide();
+      }
+    });
+    startSlide(1500);
+
+  };
+
+  slider();
 
 });
