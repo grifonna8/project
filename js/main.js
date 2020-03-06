fc5c1f0d767acb762441bba5a3206cf87793e983
grifonna8 <grifonna8@gmail.com>
2020-03-06 20:31:22 +0300

    added animation in popup

--- js/main.js
+++ js/main.js
@@ -88,29 +88,57 @@ document.addEventListener('DOMContentLoaded', function(){
   const togglePopup = () => {
     const popup = document.querySelector('.popup'),
           popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close'),
           popupContent = document.querySelector('.popup-content'),
           width = document.documentElement.clientWidth;
-    let animInterval;
-  
+    let monitorCenter, popupContentWidth;
+    
+    // let popupAnimate = () => {
+    //   popupContent.style.left = '0';
+    //     animInterval = requestAnimationFrame(popupAnimate);
+    //     for(let i = 0; i <= width / 2; i++){
+    //       popupContent.style.left = i + 'px';
+    //     }
+    // };
+
     popupBtn.forEach((elem) => {
       elem.addEventListener('click', () => {
         popup.style.display = 'block';
+        function animate(draw, duration=1200){
+          let startTime = performance.now();
+          function step(currentTime){
+            let progress = (currentTime - startTime) / duration;
+            draw(progress);
+            if(progress < 1) {
+              requestAnimationFrame(step);
+            }
+          }
+          requestAnimationFrame(step);
+        }
+        function animateRes(begin, end){
+          animate(
+            (progress) => {
+              popupContent.style.left = Math.ceil((end - begin) * progress + begin) + 'px';
+            }
+          );
+        }
+        popupContentWidth = popupContent.offsetWidth;
+        monitorCenter = width / 2 - popupContentWidth / 2;
+        if(width > 768){
+          console.log(width);
+          animateRes(0, monitorCenter);
+          popupContent.style.left = '0';
+        }
 
       });
     });
-    popup.addEventListener('click', (event)=>{ /* если клик за пределами попапа, то закрыть его */
-      let target = event.target;
-      if(target.classList.contains('popup-close')){
-        popup.style.display = 'none';
-      } else {
-        target = target.closest('.popup-content');
-        if(!target){
-          popup.style.display = 'none';
-        } 
-      } 
+    
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
     });
-  
+
   };
+
   togglePopup();
 
   // Табы
