2d99b47f529324feb82baa9a10d08edeeda5ba9c
grifonna8 <grifonna8@gmail.com>
2020-03-11 21:52:45 +0300

    refact: wrote math floor for result in calc

--- js/main.js
+++ js/main.js
@@ -373,7 +373,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
 
         if (typeValue && squareValue){
-          total = price * typeValue * squareValue * countValue * dayValue;
+          total = Math.floor(price * typeValue * squareValue * countValue * dayValue);
         } 
         totalValue.textContent = total;
       };
