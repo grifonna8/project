82b3f63efd3625e61441f711289ead8c1c83510c
grifonna8 <grifonna8@gmail.com>
2020-03-11 11:33:32 +0300

    homework lesson22

--- js/main.js
+++ js/main.js
@@ -336,4 +336,58 @@ document.addEventListener('DOMContentLoaded', function(){
     });
   };
   validation();
+
+  // калькулятор
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcType = document.querySelector('.calc-type'),
+          calcSquare = document.querySelector('.calc-square'),
+          calcDay = document.querySelector('.calc-day'),
+          calcCount = document.querySelector('.calc-count'),
+          totalValue = document.getElementById('total');
+
+      const countSum = () => {
+        let total = 0,
+            countValue = 1,
+            dayValue = 1;
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+              squareValue = +calcSquare.value;
+
+        if(calcCount.value > 1){
+          countValue += (calcCount.value - 1) / 10;
+        }
+        if(calcDay.value && calcDay.value < 5){
+          dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10){
+          dayValue *= 1.5;
+        }
+
+        if (typeValue && squareValue){
+          total = price * typeValue * squareValue * countValue * dayValue;
+        } 
+        totalValue.textContent = total;
+      };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+
+      // 1 способ
+      // if(target.matches('.calc-type') || target.matches('.calc-square') || 
+      // target.matches('.calc-day') || target.matches('.calc-count')){
+      //   console.log(1);
+      // }
+
+      // 2 способ
+      // if (target === calcType || target === calcSquare || target === calcDay ||
+      //   target === calcCount){
+      //     console.log(1);
+      //   }
+
+      // 3 способ
+        if (target.matches('select') || target.matches('input')){
+          countSum();
+        }
+    });
+  };
+  calc(100);
 });
